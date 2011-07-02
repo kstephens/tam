e3d9f61a4561da22ce267f63e248f9bf94fcc314
Kurt Stephens <ks.github@kurtstephens.com>
2011-07-01 20:19:21 -0500

    Working Log4r example.

--- example/ex02.rb
+++ example/ex02.rb
@@ -6,13 +6,10 @@ require 'tam/log4r'
 require 'pp'
 require 'ruby-debug'
 
-h = { :a => 1, :b => 2 }
-pp h.to_json
 
 TAM::Record::Log.current.verbose = 9
 
-#TAM::Record::Process.wrap do
-begin
+TAM::Record::Process.wrap do
   logger = Log4r::Logger.new("tam_logger")
   logger.add(Log4r::IOOutputter.new("stderr_outputter", STDERR))
   o = Log4r::TamOutputter.new("tam_outputter")
@@ -23,8 +20,5 @@ begin
   logger.error do
     { :msg => "error: user failed", :user_id => 1234 }
   end
-rescue ::Exception => exc
-  $stderr.puts "exc = #{exc.inspect}\n  #{exc.backtrace * "\n  "}"
 end
 
-#end
